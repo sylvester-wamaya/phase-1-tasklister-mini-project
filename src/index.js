378ab271a098f089badd405ff9ad5a74e97d4769
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-06-25 18:26:41 +0300

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (event)=>{
+    event.preventDefault()
+    
+    newTask(event.target.taskId.value)
+    event.target.reset()
+  })
+})
+
+  function newTask(tasks){
+    let li = document.createElement("li")
+    let btn = document.createElement("button")
+    btn.addEventListener("click", (e)=>{
+      e.target.parentNode.remove()
+    })
+    
+    btn.textContent = "x"
+        li.textContent = `${tasks}  `
+        li.appendChild(btn)
+    
+    console.log(li)
+    document.querySelector("ul").appendChild(li)
+
+  }
+
+  
+
